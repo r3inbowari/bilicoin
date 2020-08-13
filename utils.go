6a1653a91feba6a262f3ce519eb3ba05681207af
r3inbowari <atuk@qq.com>
2020-08-13 22:50:52 +0800

    some change

--- utils.go
+++ utils.go
@@ -45,7 +45,7 @@ func BVCovertEnc(av string) string {
 		aj := int(math.Floor(float64(x_ / _x)))
 		r[s[i]] = string(table[aj%58])
 	}
-	return r[0] + r[1] + r[2] + r[3] + r[4] + r[5] + r[6] + r[7] + r[8] + r[9] + r[10] + r[11]
+	return strings.Join(r, "")
 }
 
 // buvid 生成
