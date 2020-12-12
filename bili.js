89258015fe7bac1a0c27b9a82ba3e558c2f6dc00
r3inbowari <atuk@qq.com>
2020-12-12 10:40:33 +0800

    change line

--- bili.js
+++ bili.js
@@ -1,1 +1,22 @@
-function generateUuidPart(c){for(var a="",b=0;b<c;b++){a+=parseInt(16*Math.random()).toString(16).toUpperCase()}return formatNum(a,c)}function generateUuid(){var d=generateUuidPart(8),b=generateUuidPart(4),c=generateUuidPart(4),g=generateUuidPart(4),f=generateUuidPart(12),a=(new Date).getTime();return d+"-"+b+"-"+c+"-"+g+"-"+f+formatNum((a%100000).toString(),5)+"infoc"}function formatNum(c,a){var b="";if(c.length<a){for(var d=0;d<a-c.length;d++){b+="0"}}return b+c};
+function generateUuidPart(c) {
+    for (var a = "", b = 0; b < c; b++) {
+        a += parseInt(16 * Math.random()).toString(16).toUpperCase()
+    }
+    return formatNum(a, c)
+}
+
+function generateUuid() {
+    var d = generateUuidPart(8), b = generateUuidPart(4), c = generateUuidPart(4), g = generateUuidPart(4),
+        f = generateUuidPart(12), a = (new Date).getTime();
+    return d + "-" + b + "-" + c + "-" + g + "-" + f + formatNum((a % 100000).toString(), 5) + "infoc"
+}
+
+function formatNum(c, a) {
+    var b = "";
+    if (c.length < a) {
+        for (var d = 0; d < a - c.length; d++) {
+            b += "0"
+        }
+    }
+    return b + c
+};
